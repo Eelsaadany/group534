b4ac45d2311f303602fa8fc46f25eb3155b8e1ad
Your NameMohamed01288134909 <mahamed123hamiud@gmail.com>
2023-08-12 08:26:34 +0200

    lol

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
+
 	printf("mohamed hamoud00");
+
+	printf("dev test");
+	printf("Mohammad Magdy");
+	printf("Yusuf Elsayed");
+    	printf("Yousef Magdy -_-");
+    	printf("Mostafa Ayman Abdelmaksoud test");
+
 	return 0 ;
 }
