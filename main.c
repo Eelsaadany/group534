6ec2c9d6c50f902df37622c80c200240fab70bf7
Youssef091721 <a24e4r226u@gmail.com>
2023-08-12 09:27:57 +0300

    test

--- main.c
+++ main.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #define NUM 10
-=======
 #define NUM_TWO 10
->>>>>>> 86db021d9dfca640302e8ac0de67de15aad4b2be
 
 int main()
 {
-
+    int i;
     printf("dev test");
     printf("Mohammad Magdy");
     printf("Yusuf Elsayed");
     printf("Yousef Magdy :)");
     printf("Mostafa Ayman Abdelmaksoud test");
     printf("Donia");
-<<<<<<< HEAD
     printf("YOSEF SAMY\n");
-    for (int i = 0; i < NUM; ++i) {
+    for ( i = 0; i < NUM; ++i)
+    {
         printf("Code Test !!!!\n");
     }
-=======
-    for(int i=0;i<NUM_TWO;i++)printf("YOSEF SAMY\n");
->>>>>>> 86db021d9dfca640302e8ac0de67de15aad4b2be
+    for( i=0; i<NUM_TWO; i++)printf("YOSEF SAMY\n");
     return 0 ;
 }
